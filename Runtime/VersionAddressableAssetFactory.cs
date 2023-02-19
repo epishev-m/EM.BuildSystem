83021646eeea6bced7f94882706aaec9a07c856b
Maxim Epishev <epishev.m@mail.ru>
2023-02-19 19:41:03 +0300

    Added BuildSystem Assistant

---
+++ Runtime/VersionAddressableAssetFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace EM.BuildSystem
+{
+
+using Foundation;
+
+public sealed class VersionAddressableAssetFactory : AddressableAssetFactory<VersionConfig>
+{
+	#region AddressableAssetFactory
+
+	protected override string Path => nameof(VersionConfig);
+
+	#endregion
+}
+
+}
---
+++ Runtime/VersionConfig.cs
@@ -0,0 +1,43 @@
+﻿namespace EM.BuildSystem
+{
+
+using UnityEngine;
+
+[CreateAssetMenu(fileName = nameof(VersionConfig), menuName = "Game/Version Config")]
+public sealed class VersionConfig : ScriptableObject,
+	IVersionConfig
+{
+	[SerializeField]
+	private string _version = "0.0.1";
+
+	[SerializeField]
+	private int _code = 1;
+
+	#region IVersion
+
+	public string Version
+	{
+		get => _version;
+#if UNITY_EDITOR
+		set
+		{
+			_version = value;
+		}
+#endif
+	}
+
+	public int Code
+	{
+		get => _code;
+#if UNITY_EDITOR
+		set
+		{
+			_code = value;
+		}
+#endif
+	}
+
+	#endregion
+}
+
+}
